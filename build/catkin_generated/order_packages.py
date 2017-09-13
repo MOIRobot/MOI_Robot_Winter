6ce52ccd9f28ad27082cd7e6ac4083fea1294c79
GITHUBFlying <2385909406@qq.com>
2017-09-13 16:29:50 +0800

    add the first robot model urdf rvis and the launch file

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/jobsg/MOI_Robot_Winter/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/jobsg/MOI_Robot_Winter/devel;/opt/ros/indigo".split(';') if "/home/jobsg/MOI_Robot_Winter/devel;/opt/ros/indigo" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/jobsg/MOI_Robot_Winter/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/jobsg/MOI_Robot_Winter/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/jobsg/MOI_Robot_Winter/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/jobsg/MOI_Robot_Winter/src:$ROS_PACKAGE_PATH"

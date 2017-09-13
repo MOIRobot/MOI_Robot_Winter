9174c8239d5173d317044240dfe3013cea70af77
GITHUBFlying <2385909406@qq.com>
2017-09-13 16:16:22 +0800

    create the first robot ros workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jobsg/MOI_Robot_Winter/src
+# Build directory: /home/jobsg/MOI_Robot_Winter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jobsg/MOI_Robot_Winter/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/jobsg/MOI_Robot_Winter/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jobsg/MOI_Robot_Winter/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/jobsg/MOI_Robot_Winter/src:$ROS_PACKAGE_PATH"

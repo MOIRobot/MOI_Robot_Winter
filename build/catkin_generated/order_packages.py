ae2c2e70329aa8df589f363fbc3c3ab7c856f6b9
GITHUBFlying <2385909406@qq.com>
2017-09-24 16:47:46 +0800

    gazebo model_caster

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/jobsg/MOI_Robot_Winter/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/jobsg/MOI_Robot_Winter/devel;/opt/ros/indigo".split(';') if "/home/jobsg/MOI_Robot_Winter/devel;/opt/ros/indigo" != "" else []
--- build/winter_voice/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jobsg/MOI_Robot_Winter/src/winter_voice
-# Build directory: /home/jobsg/MOI_Robot_Winter/build/winter_voice
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

3f41a1aa30a00f05929934c7aee91bc3a0304733
GITHUBFlying <2385909406@qq.com>
2017-10-14 09:00:05 +0000

    update

--- src/winter_socket/src/robotStateCheck.py
+++ src/winter_socket/src/robotStateCheck.py
@@ -17,6 +17,7 @@ while True:
 	try: 
 		data, addr = udpSerSock.recvfrom(BUFSIZE)  
 		print 'Robot Online!'
+		print'received message from %s >> %s' % (addr, data)  
 		udpSerSock.sendto("Confirmed!",addr)  
 	except Exception:
 		print 'Robot Off'

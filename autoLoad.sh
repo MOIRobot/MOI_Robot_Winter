1e910c910cb53d6b14fa9dee92033109c6ddda24
GITHUBFlying <2385909406@qq.com>
2017-10-14 09:34:27 +0000

    update

--- autoLoad.sh
+++ autoLoad.sh
@@ -1,3 +1,9 @@
 #! /bin/bash   
-source /opt/ros/indigo/setup.bash  
-roscore  
+#source /opt/ros/indigo/setup.bash
+#chomd +x autoLoad.sh
+#cp autoLoad.sh /etc/init.d
+#update-rc.d start-zk.sh defaults
+#cd /etc/init.d
+#sudo update-rc.d -f autoLoad.sh remove
+# gnome-session-properties
+python ~/MOI_Robot_Winter/src/winter_socket/src/robotStatePublisher.py

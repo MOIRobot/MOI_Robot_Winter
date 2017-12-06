d2d921c5034f4286f50f115c259ec7eb41b9e895
GITHUBFlying <2385909406@qq.com>
2017-12-06 18:35:09 +0000

    version:4.3.1: update autoload.sh

--- autoLoad.sh
+++ autoLoad.sh
@@ -1,4 +1,20 @@
 #! /bin/bash   
+case "$1" in
+    start)
+        echo "Starting LED Blink"
+        /home/jobsg/MOI_Robot_Winter/src/winter_socket/src/Serverrobot.py &
+        ;;
+    stop)
+        echo "Stopping ledblink"
+        #killall ledblink.py
+        kill $(ps aux | grep -m 1 'python /home/jobsg/MOI_Robot_Winter/src/winter_socket/src/Serverrobot.py' | awk '{ print $2 }')
+        ;;
+    *)
+        echo "Usage: service ledblink start|stop"
+        exit 1
+        ;;
+esac
+exit 0
 #source /opt/ros/indigo/setup.bash
 #chomd +x autoLoad.sh
 #cp autoLoad.sh /etc/init.d
@@ -6,4 +22,3 @@
 #cd /etc/init.d
 #sudo update-rc.d -f autoLoad.sh remove
 # gnome-session-properties
-python /home/jobsg/MOI_Robot_Winter/src/winter_socket/src/Serverrobot.py

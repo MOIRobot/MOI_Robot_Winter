912b76d7a488c03702746054611e33c2074c74c6
GITHUBFlying <2385909406@qq.com>
2018-01-02 19:59:17 +0000

    add realsensecamera and people_detection

---
+++ server.sh
@@ -0,0 +1,9 @@
+#! /bin/bash   
+/usr/bin/python ~/MOI_Robot_Winter/src/winter_socket/src/Serverrobot.py
+#source /opt/ros/indigo/setup.bash
+#chomd +x autoLoad.sh
+#cp autoLoad.sh /etc/init.d
+#update-rc.d start-zk.sh defaults
+#cd /etc/init.d
+#sudo update-rc.d -f autoLoad.sh remove
+# gnome-session-properties

6b62e01c5ebc3cad6393e24949661c02fb7d2d43
GITHUBFlying <2385909406@qq.com>
2018-01-28 21:52:59 +0000

    update

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt-get install ros-indigo-web-video-server ros-indigo-robot-pose-publisher ros-indigo-rosbridge-server mini-httpd
+sudo apt-get install ros-indigo-web-video-server ros-indigo-robot-pose-publisher ros-indigo-rosbridge-server mini-httpd ros-indigo-navigation*
